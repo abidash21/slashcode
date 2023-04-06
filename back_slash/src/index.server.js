e10c4c3b1485a15b8e1a7b1ecac2b173e5fa74dd
Avi Jain <jainavi1408@gmail.com>
2023-04-06 23:34:55 +0530

    test commit

--- back_slash/src/index.server.js
+++ back_slash/src/index.server.js
@@ -1,36 +1,30 @@
-const express = require('express');
-const env = require('dotenv');
+const express = require("express");
+const env = require("dotenv");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 //routes
-const userRoutes = require('./routes/user');
+const userRoutes = require("./routes/user");
 
 env.config();
 //mongodb
-//mongodb+srv://slashcodehelp:<password>@clusterslash.qrktlcm.mongodb.net/?retryWrites=true&w=majority
-mongoose.connect(`mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@clusterslash.qrktlcm.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
-{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() => {
-        console.log('Database connected');
-    });
 
 app.use(bodyParser());
-app.use('/api',userRoutes);
-// app.get('/', (req, res, next) => {
-//     res.status(200).json({
-//         message: 'Hello from Server'
-//     });
-// });
-// app.post('/data', (req, res, next) => {
-//     res.status(200).json({
-//         message: req.body
-//     });
-// });
+app.use("/api", userRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@clusterslash.qrktlcm.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+    console.log("Database connected");
+  })
+  .catch((err) => console.log(err));
